58ec66dcd74db1218a43a64f3b937b8aec4d5f76
brunett06 <brunacassiane06@gmail.com>
2025-08-12 21:13:36 -0300

    Imposto de Renda

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -3,3 +3,24 @@ string nome = Console.ReadLine();
 Console.WriteLine("Informe seu salário: ");
 decimal salario = decimal.Parse(Console.ReadLine());
 Console.WriteLine("Seja bem-vinda(o), {0} o seu salário é {1}.",nome,salario);
+if (salario <= 26963.20m)
+{
+    Console.WriteLine("Isento");
+}
+else if (salario >= 26963.21m && salario <= 33919.80m)
+{
+    Console.WriteLine($"O seu salário atual é {salario - salario * 0.075m}");
+}
+else if (salario >= 33919.81m && salario <= 45012.60m)
+{
+    Console.WriteLine($"O seu salário atual é {salario - salario * 0.15m}");
+}
+else if (salario >= 45012.61m && salario <= 55976.16m)
+{
+    Console.WriteLine($"O seu salário atual é {salario - salario * 0.225m}");
+}
+else
+{
+    Console.WriteLine($"O seu salário atual é {salario - salario * 0.275m}");
+}
+//informe o valor do IRPF a ser pago de acordo com o salário informado. Utilize a tabela real de desconto de IRPF
