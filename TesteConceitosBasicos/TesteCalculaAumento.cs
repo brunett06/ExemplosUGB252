db3be52a40211360906c93480f43cf9397a74803
brunett06 <brunacassiane6@hotmail.com>
2025-09-02 20:39:57 -0300

    TesteCalculaAumento

---
+++ TesteConceitosBasicos/TesteCalculaAumento.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConceitosBasicos.uteis;
+
+namespace TesteConceitosBasicos
+{
+    [TestClass]
+    public class TesteCalculaAumento
+    {
+        [TestMethod]
+        public void TesteCalcular()
+        {
+            // cenário
+            decimal salario = 1000;
+            float percentual = 20;
+            decimal resultado;
+            CalculaAumento calculador = new CalculaAumento();
+
+            //ação
+            resultado = calculador.Calcular(salario, percentual);
+
+            //verificação
+            Assert.AreEqual(200, resultado);
+        }
+    }
+}
